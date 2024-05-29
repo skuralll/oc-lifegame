f4c423430b5fd3a548d61bfe3246a9136ef9198b
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-05-29 15:20:15 +0900

    Add new Effect handling for calculating next board state (still just return the same board)

--- lg_server/src/main.ml
+++ lg_server/src/main.ml
@@ -1,13 +1,30 @@
+open Effect
+open Effect.Deep
 open Ppx_yojson_conv_lib.Yojson_conv.Primitives
 
 type board_state = {
   width: int;
   height: int;
-  board: int array;
+  board: int array array;
 } [@@deriving yojson]
 
+type _ Effect.t += GetNext : board_state -> board_state Effect.t
 
-let () =
+let base_rule f = 
+  match_with f () {
+    retc = (fun x -> x);
+    exnc = (fun e -> raise e);
+    effc = (fun  (type b) (eff: b t)-> 
+      match eff with
+        | GetNext (board) -> 
+          (Some (fun (k: (b,_) continuation) -> 
+            continue k board
+          ))
+        | _ -> None
+    )
+  }
+
+let main () =
   Dream.run
   @@ Dream.logger
   @@ Dream.router [
@@ -29,8 +46,10 @@ let () =
           |> Yojson.Safe.from_string
           |> board_state_of_yojson
         in
-        let next_board_state = board_state in (* TODO:次の盤面を計算する *)
+        let next_board_state = perform(GetNext board_state) in (* TODO:次の盤面を計算する *)
         
         Dream.json (Yojson.Safe.to_string (yojson_of_board_state next_board_state)));
 
   ]
+
+let _ = base_rule main
