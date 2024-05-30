01fe64b3b4bfaaf7ca3b766e3fe2a43867c3dc86
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-05-30 16:05:20 +0900

    Implemented basic life-game server with effects and effect handlers

--- lg_server/src/main.ml
+++ lg_server/src/main.ml
@@ -9,6 +9,15 @@ type board_state = {
 } [@@deriving yojson]
 
 type _ Effect.t += GetNext : board_state -> board_state Effect.t
+type _ Effect.t += IsAlive : (board_state * int * int)-> bool Effect.t
+type _ Effect.t += AroundAlives : (board_state * int * int)-> int Effect.t
+type _ Effect.t += SetCell : (board_state * int * int * int)-> unit Effect.t
+
+let arounds = [|
+  (-1,-1);( 0,-1);( 1,-1);
+  (-1, 0);( 1, 0);
+  (-1, 1);( 0, 1);( 1, 1);
+  |];;
 
 let inner_run f () =
   match_with f () {
@@ -18,21 +27,57 @@ let inner_run f () =
       match eff with
         | GetNext (board) -> 
           (Some (fun (k: (b,_) continuation) -> 
-            continue k board
+            let next_board = {
+              width = board.width;
+              height = board.height;
+              board = Array.make_matrix board.height board.width 0;
+            } in
+            for pos_y = 0 to board.height - 1 do
+              for pos_x = 0 to board.width - 1 do
+                let alive = perform(IsAlive (board, pos_x, pos_y)) in
+                let arounds = perform(AroundAlives (board, pos_x, pos_y)) in
+                let next_state = 
+                  if alive then
+                    if arounds == 2 || arounds == 3 then 1 else 0
+                  else
+                    if arounds == 3 then 1 else 0 in
+                perform(SetCell (next_board, pos_x, pos_y, next_state))
+              done
+            done;
+            continue k next_board
           ))
         | _ -> None
     )
   } 
 
 let cellgame_run f = 
+  let get_at board x y = 
+    if x < 0 || x >= board.width || y < 0 || y >= board.height then
+      0
+    else
+      board.board.(y).(x) in
   match_with (inner_run f) () {
     retc = (fun x -> x);
     exnc = (fun e -> raise e);
     effc = (fun  (type b) (eff: b t)-> 
       match eff with
-        | GetNext (board) -> 
+        | IsAlive (board, pos_x, pos_y) -> 
           (Some (fun (k: (b,_) continuation) -> 
-            continue k board
+            continue k (get_at board pos_x pos_y == 1)
+          ))
+        | AroundAlives (board, pos_x, pos_y) -> 
+          (Some (fun (k: (b,_) continuation) -> 
+            let count = ref 0 in
+            Array.iter (fun (dx, dy) ->
+              get_at board (pos_x + dx) (pos_y + dy) 
+                |> (fun x -> if x == 1 then count := !count + 1)
+            ) arounds;
+            continue k !count
+          ))
+        | SetCell (board, pos_x, pos_y, state) -> 
+          (Some (fun (k: (b,_) continuation) -> 
+            board.board.(pos_y).(pos_x) <- state;
+            continue k ()
           ))
         | _ -> None
     )
