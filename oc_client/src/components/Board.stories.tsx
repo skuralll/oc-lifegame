2a3f68b03f4fe76e8cd153f0430ddc19a234b792
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-06-06 12:33:32 +0900

    Refactor Board component and Story to use single quotes

--- oc_client/src/components/Board.stories.tsx
+++ oc_client/src/components/Board.stories.tsx
@@ -1,16 +1,28 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react';
 
-import { Board } from "./Board";
+import { Board } from './Board';
+import { BoardProvider } from '../contexts/BoardProvider';
 
 const meta: Meta<typeof Board> = {
-  title: "components/Board",
+  title: 'components/Board',
   component: Board,
-  tags: ["autodocs"],
-  args: {},
-  argTypes: {},
+  tags: ['autodocs'],
+  args: { cellSize: undefined },
+  argTypes: { cellSize: { control: 'number' } },
+  decorators: [
+    (Story) => (
+      <BoardProvider>
+        <Story />
+      </BoardProvider>
+    ),
+  ],
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    cellSize: 30,
+  },
+};
--- oc_client/src/components/Board.tsx
+++ oc_client/src/components/Board.tsx
@@ -1,3 +1,29 @@
-export const Board = () => {
-  return <>This is test</>;
+import { Layer, Rect, Stage } from 'react-konva';
+import { Cell } from './Cell';
+import { useContext } from 'react';
+import { BoardContext, BoardContextType } from '../contexts/BoardProvider';
+
+type BoardProps = {
+  cellSize: number;
+};
+
+export const Board = ({ cellSize }: BoardProps) => {
+  const { boardState } = useContext(BoardContext) as BoardContextType;
+  return (
+    <Stage width={boardState.width * cellSize} height={boardState.height * cellSize}>
+      <Layer>
+        {boardState.board.map((row, y) =>
+          row.map((alive, x) => (
+            <Cell
+              key={`${x}-${y}`}
+              x={x * cellSize}
+              y={y * cellSize}
+              cellSize={cellSize}
+              alive={alive == 1}
+            />
+          ))
+        )}
+      </Layer>
+    </Stage>
+  );
 };
