dfe4770aaccea09aea1c038546c4b1a268294623
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-06-11 15:52:36 +0900

    Add auto advance feature in LifeGame component

--- oc_client/src/components/LifeGame.tsx
+++ oc_client/src/components/LifeGame.tsx
@@ -1,17 +1,32 @@
 import { Button } from '@chakra-ui/react';
 import { Board } from './Board';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { BoardContext, BoardContextType } from '../contexts/BoardProvider';
+import { useAnimationFrame } from '../hooks/AnimationFrame';
 
 export const LifeGame = () => {
   const { resetCellState, advanceBoard } = useContext(BoardContext) as BoardContextType;
+  const [isAuto, setIsAuto] = useState(false);
 
   const handleReset = () => {
     resetCellState();
+    isAuto && toggleAutoAdvance();
   };
 
   const handleAdvance = async () => {
     await advanceBoard();
+  };
+
+  // ゲームループ
+  const gameLoop = (elapsed: number) => {
+    advanceBoard();
+  };
+  const { start: startAuto, stop: endAuto } = useAnimationFrame(gameLoop, 250);
+
+  // 自動進行切り替え
+  const toggleAutoAdvance = () => {
+    isAuto ? endAuto() : startAuto();
+    setIsAuto(!isAuto);
   };
 
   return (
@@ -19,6 +34,7 @@ export const LifeGame = () => {
       <Board cellSize={50} />
       <Button onClick={handleReset}>リセット</Button>
       <Button onClick={handleAdvance}>進める</Button>
+      <Button onClick={toggleAutoAdvance}>自動で進める</Button>
     </>
   );
 };
