a99a5722780e7f1e3dc2cccbe20a6bb11adc5620
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-08-03 15:14:51 +0900

    Add QRElement to NavBar

--- oc_client/src/components/NavBar.tsx
+++ oc_client/src/components/NavBar.tsx
@@ -1,23 +1,50 @@
 // Chakra UI を使ったNavBar
-import { Box, Flex, Spacer, Text, Link } from '@chakra-ui/react';
-import { FaGithub } from 'react-icons/fa';
+import {
+  Box,
+  Flex,
+  Spacer,
+  Text,
+  Link,
+  HStack,
+  IconButton,
+  Icon,
+  Button,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { FaGithub, FaQrcode } from 'react-icons/fa';
+import { QRModal } from './QRModal';
 
 type NavBarProps = {};
 
 export const NavBar = ({}: NavBarProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Flex bg="rgb(77,122,170)" p={4} align="center" px={12} mb={8}>
-      <Box>
-        <Text color="white" fontSize="xl" fontWeight="bold">
-          ライフゲーム
-        </Text>
-      </Box>
-      <Spacer />
-      <Box ml={4}>
-        <Link href="https://github.com/skuralll/oc-lifegame">
-          <FaGithub color="#fff" size="26px" />
-        </Link>
-      </Box>
-    </Flex>
+    <>
+      {' '}
+      <Flex bg="rgb(77,122,170)" p={4} align="center" px={12} mb={8}>
+        <Box>
+          <Text color="white" fontSize="xl" fontWeight="bold">
+            ライフゲーム
+          </Text>
+        </Box>
+        <Spacer />
+        <HStack ml={4} spacing={4}>
+          <Link href="https://github.com/skuralll/oc-lifegame">
+            <FaGithub color="#fff" size="26px" />
+          </Link>
+          <IconButton
+            aria-label="QRCode"
+            variant="ghost"
+            width={0}
+            height={0}
+            color={'white'}
+            icon={<Icon as={FaQrcode} />}
+            onClick={onOpen}
+          />
+        </HStack>
+      </Flex>
+      <QRModal isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+    </>
   );
 };
